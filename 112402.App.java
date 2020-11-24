4416e051d38bb7f0bf855b2a74d39f44ca2d2cad
Ljn1124 <74773427+Ljn1124@users.noreply.github.com>
2020-11-24 20:57:20 +0800

    提交

---
+++ 112402.App.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class App {
+    JPanel myPanel=new JPanel();
+    JTextArea textArea_A=new JTextArea();
+    JTextArea textArea_B=new JTextArea();
+    //构造方法
+    public App() {
+        textArea_A.setBounds(100,100,256,128);
+        textArea_A.setBackground(Color.green);
+        myPanel.add(textArea_A);
+        textArea_B.setBounds(100,300,256,128);
+        textArea_B.setBackground(Color.yellow);
+        myPanel.add(textArea_B);
+
+
+        textArea_A.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                super.keyPressed(e);
+                if (e.getKeyCode()==KeyEvent.VK_ENTER){
+                    if (textArea_A.getText().equals("周树人")){
+                        textArea_B.setText("输入正确");
+                    }else{
+                        textArea_B.setText("输入错误");
+                    }
+                }
+            }
+        });
+    }
+    //显示窗体方法
+    void go(){
+        JFrame frame = new JFrame("TestGUI");
+        myPanel.setLayout(null);
+        frame.setContentPane(myPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100,100,512,720);
+        //frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new App().go();
+    }
+}
